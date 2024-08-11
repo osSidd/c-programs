5eb01615a67b992a81d1a8f873e129cacf3dbd4e
Osama <107386980+osSidd@users.noreply.github.com>
2024-08-11 22:30:06 +0530

    simple cricket data record

--- student management/file.c
+++ student management/file.c
@@ -1,39 +1,26 @@
 #include<stdio.h>
-#include<string.h>
 
-struct automobile {
-        char serial_no[4];
-        int yr_of_manu;
-        char material[15];
-        int qty;
-    } a1[8] = {
-        "AA0", 1991, "Aluminium", 52,
-        "AA1", 1995, "Alumina", 55,
-        "BB0", 1995, "Iron", 51,
-        "DD1", 1992, "Zirconium", 25,
-        "CC0", 1991, "Carbon fiber", 65,
-        "BB1", 1997, "Zinc", 45,
-        "DD0", 1998, "Bronze", 35,
-        "CC1", 1999, "Copper", 55,
-    };
+struct cric_record {
+    char name[20];
+    int age;
+    int test_match;
+    float avg_runs;
+};
 
-void retrieve(char *, char *);
+struct cric_record records[5] = {
+    "Sunil Gavaskar", 32, 200, 200,
+    "Sachin Tendulkar", 33, 300, 132.2,
+    "Saurav Ganguly", 21, 200, 153.2,
+    "Don Bradman", 43, 400, 211.2,
+    "Brian Lara", 44, 420, 235.5 
+};
 
 void main(){
-    retrieve("AA0", "BB0");
-}
+    struct cric_record *p;
+    p = records;
 
-void retrieve(char *p1, char *p2){
-    struct automobile *p;
-    p = a1;
-
-    for(int i = 0; i<=7; i++){
-        p = a1 + i;
-    
-        int compare1 = strcmp(p1, p->serial_no);
-        int compare2 = strcmp(p2, p->serial_no);
-
-        if(compare1 <= 0 && compare2 >= 0)    
-            printf("Entered parameters %s %d %s %d\n", p->serial_no, p->yr_of_manu, p->material, p->qty);
+    for(int i = 0; i<=4; i++){
+        printf("%s %d %d %.1f\n", p->name, p->age, p->test_match, p->avg_runs);
+        p += 1;
     }
 }
