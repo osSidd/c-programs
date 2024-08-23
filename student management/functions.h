d68b9cbc0efb6657d23d5b1233843ec6372d69a2
Osama <107386980+osSidd@users.noreply.github.com>
2024-08-23 12:13:30 +0530

    chapter 1 ex complete

--- student management/functions.h
+++ student management/functions.h
@@ -1,6 +1,21 @@
+
 void functions();
 
 void functions(){
-    int k = 32;
-    printf("%d %d %d %d", k = 2, k = 50, k < 44, k > 40);
+    int num, rev_num = 0, i = 10 * 1000;
+
+    printf("enter a five digit number ");
+    scanf("%d", &num);
+
+    int original = num;
+
+    while(num){
+        rev_num += (num % 10) * i;
+        i /= 10;
+        num /= 10;
+    }
+
+    printf("The original number = %d and reversed number = %d\n", original, rev_num);
+
+    (original == rev_num) ? printf("Both are equal") : printf("Not equal");
 }
