7b01153b388542bc3fe72d2168eb2b149f570bc1
Osama <107386980+osSidd@users.noreply.github.com>
2024-09-08 13:46:10 +0530

    gcd Euclid's algo using recursion

--- student management/functions.h
+++ student management/functions.h
@@ -1,13 +1,11 @@
 void functions();
+int gcd(int, int);
 
 void functions(){
     printf("Enter two numbers");
-    int num1, num2;
+    int num1, num2, temp;
 
     scanf("%d %d", &num1, &num2);
-
-    int rem, temp;
-    int sub_rem;
 
     printf("Greatest common divisor of %d and %d ", num1, num2);
 
@@ -16,17 +14,18 @@ void functions(){
         num1 = num2;
         num2 = temp;
     }
+    printf("is %d", gcd(num1, num2));
+}
+
+int gcd(int num1, int num2){
+    int rem, temp;
+    int sub_rem;
 
     rem = num1 / num2;
     sub_rem = num1 - (rem * num2);
 
-    while(sub_rem){
-        num1 = num2;
-        num2 = sub_rem;
-
-        rem = num1/num2;
-        sub_rem = num1 - (rem * num2);
-    }
-
-    printf("is %d", num2);
+    if(!sub_rem)
+        return num2;
+    
+    return gcd(num2, sub_rem);
 }
